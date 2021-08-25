6495e46312fec95ab817e2e73d1506a3f248c153
joue-zero <elnagaryoussef864@gmail.com>
2021-08-25 15:18:33 +0200

    Make The Galary

--- js/plugin.js
+++ js/plugin.js
@@ -1,4 +1,4 @@
-// For Make TH Height Of Landig Page Full Screen
+// For Make The Height Of Landig Page Full Screen
 
 $upHeight = $(".upper-nav").innerHeight();
 $navHeight = $(".navbar").innerHeight();
@@ -6,5 +6,18 @@ $land = $(".landing").css(
   "height",
   window.innerHeight - ($upHeight + $navHeight)
 );
+
+// Galary 
+$('.galary ul li').on('click', function () {
+  $(this).addClass('active').siblings().removeClass('active');
+
+  if ($(this).data('class') === 'all') {
+    $('.galary-image div.img-box').css('opacity', '1');
+  }else {
+    $('.galary-image div.img-box').css('opacity', '.09');
+    $($(this).data('class')).parent().css('opacity', '1');
+  }
+});
+
 
 
